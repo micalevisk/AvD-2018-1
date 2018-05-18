416c6f0e2a5036a256c80b7217cc6d2679ee3ae7
Micael Lab <mllc@icomp.ufam.edu.br>
2018-05-18 14:15:40 -0400

    propósito redefinido

--- trab1/etapa1/gerar_arquivos_normalizados.sh
+++ trab1/etapa1/gerar_arquivos_normalizados.sh
@@ -8,96 +8,17 @@
 # o separador TAB se misturava com espaços.
 # Este script irá criar uma versão normalizada
 # onde o separador será o ponto-e-vírgula (alguns valores possuem espaços).
-# Será criado um diretório de nome "SourceNormalizados"
-# e os arquivos `.dat` serão salvos (normalizados) como `.csv`
+# Será gerado um arquivo "SocialsStrengthNormalizado.csv" com
+# os campos de `SocialStrength.dat` devidamente separados.
 #
 
-for i in Experiment-*/USense*/**/*.dat
+for i in Experiment-*/USense*/**/SocialStrength.dat
 do
-  novoDir="${i%/*}Normalizados/"
+  novoDir="${i%/*}"
+  novoDir="${novoDir%/*}"
   arquivoOriginal="${i##*/}"
-  arquivoNormalizado="${novoDir}${arquivoOriginal/%dat/csv}"
+  arquivoNormalizado="${novoDir}/${arquivoOriginal/%.dat/Normalizado.csv}"
 
-  mkdir -p "$novoDir"
   sed -r 's/^\s*// ; s/\s+\t/\t/ ; s/\t\s/\t/ ; s/  -  /-/' "$i" | tr '\t' ';' > "$arquivoNormalizado"
   echo "Criado: '$arquivoNormalizado'"
 done
-
-## ANTES
-: '
-.
-├── Experiment-1
-│   ├── USense2
-│   │   └── Source
-│   ├── USense3
-│   │   └── Source
-│   ├── USense4
-│   │   └── Source
-│   └── USense5
-│       └── Source
-├── Experiment-2
-│   ├── USense2
-│   │   └── Source
-│   ├── USense3
-│   │   └── Source
-│   ├── USense4
-│   │   └── Source
-│   └── USense5
-│       └── Source
-└── Experiment-3
-    ├── USense2
-    │   └── Source
-    ├── USense3
-    │   └── Source
-    ├── USense4
-    │   └── Source
-    └── USense5
-        └── Source
-
-27 directories
-'
-
-## DEPOIS
-: '
-.
-├── Experiment-1
-│   ├── USense2
-│   │   ├── Source
-│   │   └── SourceNormalizados
-│   ├── USense3
-│   │   ├── Source
-│   │   └── SourceNormalizados
-│   ├── USense4
-│   │   ├── Source
-│   │   └── SourceNormalizados
-│   └── USense5
-│       ├── Source
-│       └── SourceNormalizados
-├── Experiment-2
-│   ├── USense2
-│   │   ├── Source
-│   │   └── SourceNormalizados
-│   ├── USense3
-│   │   ├── Source
-│   │   └── SourceNormalizados
-│   ├── USense4
-│   │   ├── Source
-│   │   └── SourceNormalizados
-│   └── USense5
-│       ├── Source
-│       └── SourceNormalizados
-└── Experiment-3
-    ├── USense2
-    │   ├── Source
-    │   └── SourceNormalizados
-    ├── USense3
-    │   ├── Source
-    │   └── SourceNormalizados
-    ├── USense4
-    │   └── Source
-    └── USense5
-        ├── Source
-        └── SourceNormalizados
-
-38 directories
-'
