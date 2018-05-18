522a554156e159179d18bc3c020ca846e7288096
Micael Lab <mllc@icomp.ufam.edu.br>
2018-05-18 19:08:29 -0400

    add ordenação pelo timestamp; inserção do campo `action`; normalização dos ids; remoção dos campos finais (desnecessários)

--- trab1/etapa1/unir_todos_SocialStrength.sh
+++ trab1/etapa1/unir_todos_SocialStrength.sh
@@ -7,12 +7,16 @@
 # Como os dados originais dividem as conexões de cada nó (device)
 # em diretórios diferentes, esse script reunirá todos os
 # "SocialStrength.csv" gerados após a normalização,
-# de acordo com cada experimento.
+# de acordo com cada experimento, e ordenados pelo "TimeStamp".
+# Além de inserir a coluna de "action" que é sempre "CONN",
+# alterar o nome dos id "Usense2", etc, para o formato numérico pedido na questão 1,
+# e remover as duas últimas colunas por não serem úteis para o resultado final.
 #
 
 arquivosCriados=()
 QTD_USENSES=4
 i=1
+DECREMENTO=2
 
 for f in Experiment-*/USense*/SocialStrengthNormalizado.csv
 do
@@ -20,11 +24,13 @@ do
   currUSenseNumber="${f#*/}"
   currUSenseNumber="${currUSenseNumber%%/*}"
   currUSenseNumber="${currUSenseNumber:$(( ${#currUSenseNumber}-1 )):1}"
+  currUSenseNumber=$(( currUSenseNumber - $DECREMENTO )) # transformando em ID
   tmpFile="${f}~"
 
-  # FIXME: algumas instâncias não foram tratadas
-  sed -r "s/[^;]+;/\0Usense${currUSenseNumber};/" "$f" 1> "$tmpFile" && \
-  echo "Criado: '$tmpFile'"
+  sed -r "s/[^;]+/\0;CONN;${currUSenseNumber}/" "$f" |
+  awk -F ';' -v d=$DECREMENTO 'BEGIN {OFS=";"} { $4=substr($4,7); $4-=d }1' |
+  cut -d ';' -f-6 1> "$tmpFile" && echo "Criado arquivo temporário: '$tmpFile'"
+
   arquivosCriados+=( "$tmpFile" )
 
   [[ $i -eq $QTD_USENSES ]] && {
@@ -32,7 +38,8 @@ do
     arquivosMesclados="$currExperimentPath/SocialsStrength.csv"
 
     cat "${arquivosCriados[@]}" 1> "$arquivosMesclados"
-    echo -e "\nArquivos reunidos em: '$arquivosMesclados'"
+    sort -o "$arquivosMesclados" -t'/' -k2M -k1n "$arquivosMesclados"
+    echo -e "\nArquivos reunidos e ordenados em: '$arquivosMesclados'"
   }
 
   ((++i))
@@ -45,5 +52,5 @@ find Experiment-*/USense*/ -name '*.csv~' -type f -delete
 echo -e "\nQuantidade de linhas em cada arquivo"
 wc -l Experiment-*/SocialsStrength.csv | head -3
 
-echo -e "\nQuantidade de linhas em cada arquivo gerado que contém 7 ocorrências de ponto-e-vírgula"
-for f in Experiment-*/SocialsStrength.csv; do sed -nr 's/;/;/6p' "$f" | wc -l; done
+echo -e "\nQuantidade de linhas em cada arquivo gerado que contém 5 ocorrências de ponto-e-vírgula"
+for f in Experiment-*/SocialsStrength.csv; do sed -nr 's/;/;/5p' "$f" | wc -l; done
