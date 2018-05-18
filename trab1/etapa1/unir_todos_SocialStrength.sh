b0211de4bdf44f04fd27bd4dd775f6e7eed32b96
Micael Lab <mllc@icomp.ufam.edu.br>
2018-05-18 14:16:12 -0400

    atualizado de acordo com os arquivos gerados & verificando união

--- trab1/etapa1/unir_todos_SocialStrength.sh
+++ trab1/etapa1/unir_todos_SocialStrength.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# (c) Micael Levi
+#
+# Deve ser executado no diretório "social-interaction".
+# E irá afetar todos os 3 experimentos.
+#
+# Como os dados originais dividem as conexões de cada nó (device)
+# em diretórios diferentes, esse script reunirá todos os
+# "SocialStrength.csv" gerados após a normalização,
+# de acordo com cada experimento.
+#
 
 arquivosCriados=()
 QTD_USENSES=4
-i=0
+i=1
 
-for f in Experiment-*/USense*/SourceNormalizados/SocialStrength.csv
+for f in Experiment-*/USense*/SocialStrengthNormalizado.csv
 do
-  ((++i))
-
   currExperimentPath="${f%%/*}"
   currUSenseNumber="${f#*/}"
   currUSenseNumber="${currUSenseNumber%%/*}"
@@ -20,9 +28,22 @@ do
   arquivosCriados+=( "$tmpFile" )
 
   [[ $i -eq $QTD_USENSES ]] && {
-    i=0
-    cat "${arquivosCriados[@]}" 1> "$currExperimentPath/SocialsStrength.csv"
+    i=1
+    arquivosMesclados="$currExperimentPath/SocialsStrength.csv"
+
+    cat "${arquivosCriados[@]}" 1> "$arquivosMesclados"
+    echo -e "\nArquivos reunidos em: '$arquivosMesclados'"
   }
+
+  ((++i))
 done
 
-find Experiment-*/**/SourceNormalizados -name '*.csv~' -delete
+## Apagar csv "temporários"
+find Experiment-*/USense*/ -name '*.csv~' -type f -delete
+
+## Para conferir se todas as linhas possuem 6 ocorrências de ponto-e-vírgula
+echo -e "\nQuantidade de linhas em cada arquivo"
+wc -l Experiment-*/SocialsStrength.csv | head -3
+
+echo -e "\nQuantidade de linhas em cada arquivo gerado que contém 7 ocorrências de ponto-e-vírgula"
+for f in Experiment-*/SocialsStrength.csv; do sed -nr 's/;/;/6p' "$f" | wc -l; done
