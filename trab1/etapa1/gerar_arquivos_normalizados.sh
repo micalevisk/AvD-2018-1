1f7b108751234833ae4bd2fb0c2ec6723387dc1b
Micael Levi <mllc@icomp.ufam.edu.br>
2018-05-19 01:36:42 -0400

    lógica alterada & corriga para o problema dos tempos em segundos

--- trab1/etapa1/gerar_arquivos_normalizados.sh
+++ trab1/etapa1/gerar_arquivos_normalizados.sh
@@ -1,26 +1,51 @@
 #!/bin/bash
-# (c) Micael Levi
+# 18-05-2018 (c) Micael Levi L. C.
 #
 # Deve ser executado no diretório "social-interaction".
 # E irá afetar todos os 3 experimentos.
 #
 # Nos dados originais, em alguns casos
 # o separador TAB se misturava com espaços.
-# Este script irá criar uma versão normalizada
-# onde o separador será o ponto-e-vírgula (alguns valores possuem espaços),
-# os registros com duração 0 e onde o DeviceName não é um dos usenses serão removidos.
+# Este script irá criar uma versão normalizada/corrigida
+# onde o separador será o ponto-e-vírgula (alguns valores dos campos possuem espaços)
+# e os campos desnecessários (os 2 últimos do arquivo original) removidos.
+#
 # Será gerado um arquivo "SocialsStrengthNormalizado.csv" com
-# os campos de `SocialStrength.dat` devidamente separados.
+# os campos de "SocialStrength.dat" devidamente separados.
 #
 
-for i in Experiment-*/USense*/**/SocialStrength.dat
+: '
+social-interaction
+  │
+  ├── Experiment-1
+  │   ├── USense2
+  │   │   ├── SocialStrengthNormalizado.csv *
+  │   │   └── Source
+  │   ├── USense3
+  │   │   ├── SocialStrengthNormalizado.csv *
+  │   │   └── Source
+  │   ├── USense4
+  │   │   ├── SocialStrengthNormalizado.csv *
+  │   │   └── Source
+  │   └── USense5
+  │       ├── SocialStrengthNormalizado.csv *
+  │       └── Source
+  ...
+'
+
+
+SEP=';'
+
+for i in Experiment-[0-9]/USense[0-9]/**/SocialStrength.dat
 do
   novoDir="${i%/*}"
   novoDir="${novoDir%/*}"
   arquivoOriginal="${i##*/}"
   arquivoNormalizado="${novoDir}/${arquivoOriginal/%.dat/Normalizado.csv}"
 
-  sed -r 's/^\s*// ; s/\s+\t/\t/ ; s/\t\s/\t/ ; s/  -  /-/' "$i" | tr '\t' ';' > "$arquivoNormalizado"
-  sed -ri '/^[^;]+;[^;]+;0;/d ; /^[^;]+;Usense[2-5]/!d' "$arquivoNormalizado"
+  sed -r 's/^\s*// ; s/\s+\t/\t/ ; s/\t\s/\t/ ; s/  -  /-/' "$i" | ## apagando espaços excedentes
+  tr '\t' $SEP | ## alterando o separador padrão TAB
+  cut -d $SEP -f-4 1> "$arquivoNormalizado" ## recuperar apenas as 4 primeiras colunas
+
   echo "Criado: '$arquivoNormalizado'"
 done
--- trab1/etapa1/unir_todos_SocialStrength.sh
+++ trab1/etapa1/unir_todos_SocialStrength.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# (c) Micael Levi
+# 18-05-2018 (c) Micael Levi L. C.
 #
 # Deve ser executado no diretório "social-interaction".
 # E irá afetar todos os 3 experimentos.
@@ -7,52 +7,77 @@
 # Como os dados originais dividem as conexões de cada nó (device)
 # em diretórios diferentes, esse script reunirá todos os
 # "SocialStrength.csv" gerados após a normalização,
-# de acordo com cada experimento, e ordenados pelo "TimeStamp".
+# de acordo com cada experimento, e ordenados pelo "TimeStamp" que será convertido
+# para o formato de instante de tempo em segundos, i.e., o primeiro horário registrado
+# terá o tempo 60 segundos, o segundo (1 minuto depois), terá 120, e assim por diante.
 # Além de inserir a coluna de "action" que é sempre "CONN",
-# alterar o nome dos id "Usense2", etc, para o formato numérico pedido na questão 1,
-# e remover as duas últimas colunas por não serem úteis para o resultado final.
+# alterar o nome dos id "Usense2", etc, para o formato numérico de ID de nó (i.e, Usense2 virará 0).
+#
+# Será gerado um arquivo "SocialsStrength.csv" no diretório raiz de cada experimento.
 #
 
-arquivosCriados=()
-QTD_USENSES=4
-i=1
-DECREMENTO=2
+: '
+social-interaction
+  │
+  ├── Experiment-1
+  │   ├── SocialsStrength.csv *
+  │   ├── USense2
+  │   │   └── Source
+  │   ├── USense3
+  │   │   └── Source
+  │   ├── USense4
+  │   │   └── Source
+  │   └── USense5
+  │       └── Source
+  ...
+'
 
-for f in Experiment-*/USense*/SocialStrengthNormalizado.csv
+
+SEP=';'
+DECREMENTO=2 ## para normalizar os nomes `UsenseX` em IDs de acordo com o `X` (valor de 2 a 5)
+i=1 ## contador de arquivos normalizados para cada `Experiment`
+arquivosCriados=() ## array de caminhos para os arquivos gerados
+
+for f in Experiment-[0-9]/USense[2-5]/SocialStrengthNormalizado.csv
 do
+  [[ -r "$f" ]] || exit ${1?'arquivo ausente ou sem permissão para leitura'}
+
+  tmpFile="${f}~"
   currExperimentPath="${f%%/*}"
   currUSenseNumber="${f#*/}"
   currUSenseNumber="${currUSenseNumber%%/*}"
   currUSenseNumber="${currUSenseNumber:$(( ${#currUSenseNumber}-1 )):1}"
   currUSenseNumber=$(( currUSenseNumber - $DECREMENTO )) # transformando em ID
-  tmpFile="${f}~"
 
-  sed -r "s/[^;]+/\0;CONN;${currUSenseNumber}/" "$f" |
-  awk -F ';' -v d=$DECREMENTO 'BEGIN {OFS=";"} { $4=substr($4,7); $4-=d }1' |
-  cut -d ';' -f-6 1> "$tmpFile" && echo "Criado arquivo temporário: '$tmpFile'"
+  sed -r "s/[^;]+/\0;CONN;${currUSenseNumber}/" "$f" | ## inserir os 2 campos faltantes, `action` e `first_node_ID`
+  awk -F $SEP -v d=$DECREMENTO '$4~/Usense[2-5]/ { $4=substr($4,7); $4-=d }1' OFS=$SEP > "$tmpFile" ## transformar os DeviceName dos segundos nós em ID
+  ##                                                                 ^^^ vai dar `-2` quando o `DeviceName` não for válido
+  echo "Criado arquivo (temporário): '$tmpFile'"
 
   arquivosCriados+=( "$tmpFile" )
-  rm -f "$f"
+  rm -f "$f" ## apagar a versão normalizada antiga
   echo -e "Arquivo '$f' removido"
 
-  [[ $i -eq $QTD_USENSES ]] && {
+  if [[ $i -eq 4 ]]; then
     i=1
     arquivosMesclados="$currExperimentPath/SocialsStrength.csv"
 
-    cat "${arquivosCriados[@]}" 1> "$arquivosMesclados"
+    cat "${arquivosCriados[@]}" 1> "$arquivosMesclados" &&
     sort -o "$arquivosMesclados" -t'/' -k2M -k1n "$arquivosMesclados"
-    echo -e "\nArquivos reunidos e ordenados em: '$arquivosMesclados'"
-  }
+
+    ## converter timestamp para tempos em segundos
+    gawk -i inplace -F $SEP 'BEGIN {t=0} { if (!a[$1]){ t+=60; a[$1]=1 }; $1=""; print t $0 }' OFS=$SEP "$arquivosMesclados"
+
+    ## remover linhas onde o campo `DeviceName` não é um dos IDs e o `Encounter Duration` é diferente de 0
+    gawk -i inplace -F $SEP '$4~/^[0-4]$/ && $5 != "0"' "$arquivosMesclados"
+    ##                            ^^^^^ está no range dos `Usensex` que foram convertidos para IDs
+
+    echo -e "Arquivos reunidos e ordenados em: '$arquivosMesclados'\n"
+    ## FORMATO: tempo;action;first_node_ID;second_node_ID;encounter_duration;average_encounter_duration
+  fi
 
   ((++i))
 done
 
 ## Apagar csv "temporários"
-find Experiment-*/USense*/ -name '*.csv~' -type f -delete
-
-## Para conferir se todas as linhas possuem 6 ocorrências de ponto-e-vírgula
-echo -e "\nQuantidade de linhas em cada arquivo"
-wc -l Experiment-*/SocialsStrength.csv | head -3
-
-echo -e "\nQuantidade de linhas em cada arquivo gerado que contém 5 ocorrências de ponto-e-vírgula"
-for f in Experiment-*/SocialsStrength.csv; do sed -nr 's/;/;/5p' "$f" | wc -l; done
+find Experiment-[0-9]/USense[2-5]/ -name '*Normalizado.csv~' -type f -delete
