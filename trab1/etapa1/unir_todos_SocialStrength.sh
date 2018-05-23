543842bcf39f8017050705a09767642a6254e187
Micael Levi <mllc@icomp.ufam.edu.br>
2018-05-22 22:53:59 -0400

    up: mantém o timestamp, nós com duração 0 e nós que não são usense (mas convertidos)

--- trab1/etapa1/unir_todos_SocialStrength.sh
+++ trab1/etapa1/unir_todos_SocialStrength.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 18-05-2018 (c) Micael Levi L. C.
+# 22-05-2018 (c) Micael Levi L. C.
 #
 # Deve ser executado no diretório "social-interaction".
 # E irá afetar todos os 3 experimentos.
@@ -7,9 +7,7 @@
 # Como os dados originais dividem as conexões de cada nó (device)
 # em diretórios diferentes, esse script reunirá todos os
 # "SocialStrength.csv" gerados após a normalização,
-# de acordo com cada experimento, e ordenados pelo "TimeStamp" que será convertido
-# para o formato de instante de tempo em segundos, i.e., o primeiro horário registrado
-# terá o tempo 60 segundos, o segundo (1 minuto depois), terá 120, e assim por diante.
+# de acordo com cada experimento, e ordenados pelo "TimeStamp".
 # Além de inserir a coluna de "action" que é sempre "CONN",
 # alterar o nome dos id "Usense2", etc, para o formato numérico de ID de nó (i.e, Usense2 virará 0).
 #
@@ -49,12 +47,21 @@ do
   currUSenseNumber="${currUSenseNumber:$(( ${#currUSenseNumber}-1 )):1}"
   currUSenseNumber=$(( currUSenseNumber - DECREMENTO )) # transformando em ID
 
-  sed -r "s/[^;]+/\0;CONN;${currUSenseNumber}/" "$f" | ## inserir os 2 campos faltantes, `action` e `first_node_ID`
-  awk -F $SEP -v d=$DECREMENTO '$4~/Usense[2-5]/ { $4=substr($4,7); $4-=d }1' OFS=$SEP > "$tmpFile" ## transformar os DeviceName dos segundos nós em ID
-  ##                                                                 ^^^ vai dar `-2` quando o `DeviceName` não for válido
-  echo "Criado arquivo (temporário): '$tmpFile'"
+  echo -e "\n-------- ${f%/*} --------"
 
+  sed -r "s/[^;]+/\0;CONN;${currUSenseNumber}/" "$f" | ## inserir os 2 campos faltantes, `action` e `first_node_ID`
+  awk -F $SEP -v d=$DECREMENTO 'BEGIN{ l=4 } {
+    if ($4 ~ /^Usense[2-5]/) { ## se o campo tiver o valor iniciando com "Usense" seguido por um número
+      $4 = substr($4, 7) - d   ## então ele receberá apenas o valor numérico subtraído de "d"
+    } else {
+      if (!n[$4]) { n[$4] = l++ }
+      $4 = n[$4]
+    }
+  }1' OFS=$SEP > "$tmpFile" ## transformar os DeviceName dos segundos nós em ID
+
+  echo "Criado arquivo (temporário): '$tmpFile'"
   arquivosCriados+=( "$tmpFile" )
+
   rm -f "$f" ## apagar a versão normalizada antiga
   echo -e "Arquivo '$f' removido"
 
@@ -62,22 +69,16 @@ do
     i=1
     arquivosMesclados="$currExperimentPath/SocialsStrength.csv"
 
+    echo "Mesclando os arquivos e ordenando pelo 'Timestamp'"
     cat "${arquivosCriados[@]}" 1> "$arquivosMesclados" &&
     sort -o "$arquivosMesclados" -t'/' -k2M -k1n "$arquivosMesclados"
 
-    ## converter timestamp para tempos em segundos
-    gawk -i inplace -F $SEP 'BEGIN {t=-60} { if (!a[$1]){ t+=60; a[$1]=1 }; $1=""; print t $0 }' OFS=$SEP "$arquivosMesclados"
-
-    ## remover linhas onde o campo `DeviceName` não é um dos IDs e o `Encounter Duration` é diferente de 0
-    gawk -i inplace -F $SEP '$4~/^[0-4]$/ && $5 != "0"' "$arquivosMesclados"
-    ##                            ^^^^^ está no range dos `Usensex` que foram convertidos para IDs
-
+    sed -i '1itimestamp;action;first_node_ID;second_node_ID;encounter_duration;average_encounter_duration' "$arquivosMesclados"
     echo -e "Arquivos reunidos e ordenados em: '$arquivosMesclados'\n"
-    ## FORMATO: tempo;action;first_node_ID;second_node_ID;encounter_duration;average_encounter_duration
   fi
 
   ((++i))
 done
 
-## Apagar csv "temporários"
+## Apagar csv "temporários" gerados para cada Experiment
 find Experiment-[0-9]/USense[2-5]/ -name '*Normalizado.csv~' -type f -delete
